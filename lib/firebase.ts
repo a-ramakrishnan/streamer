c43602eab733a76c151f706f7a2766577464bffb
a-ramakrishnan <62995911+a-ramakrishnan@users.noreply.github.com>
2022-11-21 15:33:02 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,18 +9,27 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAhgtcOCRKy9iXgItiZ7OIL9sF_SZSDnzc",
+
+//   authDomain: "streamer-e6921.firebaseapp.com",
+
+//   projectId: "streamer-e6921",
+
+//   storageBucket: "streamer-e6921.appspot.com",
+
+//   messagingSenderId: "950284173075",
+
+//   appId: "1:950284173075:web:59f83e833bdeea5393e4e6",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAhgtcOCRKy9iXgItiZ7OIL9sF_SZSDnzc",
-
-  authDomain: "streamer-e6921.firebaseapp.com",
-
-  projectId: "streamer-e6921",
-
-  storageBucket: "streamer-e6921.appspot.com",
-
-  messagingSenderId: "950284173075",
-
-  appId: "1:950284173075:web:59f83e833bdeea5393e4e6",
+  apiKey: "AIzaSyDo8h3lUAEQHaUL7z_Q1E2a66CxUk74a6w",
+  authDomain: "netflix-clone-a0797.firebaseapp.com",
+  projectId: "netflix-clone-a0797",
+  storageBucket: "netflix-clone-a0797.appspot.com",
+  messagingSenderId: "174208251393",
+  appId: "1:174208251393:web:36f667740ea8744dbdf200"
 };
 
 // Initialize Firebase
